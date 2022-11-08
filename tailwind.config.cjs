5c7225c9fdae69cd750831660240af30cce031a4
Elhya13 <aureliewinkin@gmail.com>
2022-11-08 15:55:59 +0100

    voila tes id valentin!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
+    "./*.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
+
+  // active card 
+  variants: 
+    ['reponsive', 'hover', 'focus','active'],
+    
   plugins: [],
 }
