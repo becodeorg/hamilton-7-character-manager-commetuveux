13f4a69a71f81c9df80e44e3697bdf3520b7c9bc
GeorisVal <georis.val@gmail.com>
2022-11-09 18:11:22 +0100

    Updated nicknames pull

--- src/js/manager.js
+++ src/js/manager.js
@@ -19,6 +19,7 @@ async function getCharacters() {
         console.log(Node)
         Node.querySelector("#card_image").src = `data:image/gif;base64,${chars[i].image}`;
         Node.querySelector("#card_charName").innerText = `${chars[i].name}`;
+        Node.querySelector("#card_nickName").innerText = `${chars[i].shortDescription}`;
         Node.querySelector("#card_description").innerHTML = `${chars[i].description}`;
         Node.querySelector("#readMore_button").href = `card.html?${chars[i].id}`;
         Node.querySelector("#readMore_button").innerText = `See the Character`;
