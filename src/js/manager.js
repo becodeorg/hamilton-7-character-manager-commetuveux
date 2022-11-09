0804828e4e57405975bdb5c15399f0651686e415
GeorisVal <georis.val@gmail.com>
2022-11-09 21:45:02 +0100

    Cleaned comments

--- src/js/manager.js
+++ src/js/manager.js
@@ -1,4 +1,18 @@
 import axios from "axios";
+import Swiper, {Pagination} from "swiper";
+import 'swiper/css';
+import 'swiper/css/pagination';
+const swiper = document.querySelector(".swiper");
+new Swiper('.swiper', {
+    slidesPerView: 3,
+    modules: [Pagination],
+    direction: 'vertical', 
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    }
+});
+
 const container = document.querySelector('template');
 
 window.onload = getCharacters();
@@ -6,17 +20,8 @@ window.onload = getCharacters();
 async function getCharacters() {
     const resp = await axios.get('https://character-database.becode.xyz/characters');
     const chars = await resp.data;
-    console.log(chars.length)
-    console.log(chars)
-/*     const charName = document.querySelector('#card_charName');
-    const charNickname = document.querySelector('#card_nickName');
-    const charDesc = document.querySelector('#card_description');
-    const charImg = document.querySelector('#card_image'); */
-    console.log('script2 ok')
     for (let i = 0; i < chars.length; i++) {
-        console.log('loop start ok')
         let Node = document.importNode(container.content, true);
-        console.log(Node)
         Node.querySelector("#card_image").src = `data:image/gif;base64,${chars[i].image}`;
         Node.querySelector("#card_charName").innerText = `${chars[i].name}`;
         Node.querySelector("#card_nickName").innerText = `${chars[i].shortDescription}`;
@@ -24,8 +29,5 @@ async function getCharacters() {
         Node.querySelector("#readMore_button").href = `card.html?${chars[i].id}`;
         Node.querySelector("#readMore_button").innerText = `See the Character`;
         document.body.appendChild(Node)
-        console.log('loop end ok')
     }
 }
-console.log('script ok')
-document.querySelector('#readMore_button').addEventListener('click', getCharacters)
