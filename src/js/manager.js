313340588116a33214b05d0f54be3c88ace8ba57
GeorisVal <georis.val@gmail.com>
2022-11-10 10:36:40 +0100

    Begin working on catch

--- src/js/manager.js
+++ src/js/manager.js
@@ -5,8 +5,10 @@ const container = document.querySelector('template');
 window.onload = getCharacters();
 
 async function getCharacters() {
+    try {
     const resp = await axios.get('https://character-database.becode.xyz/characters');
     const chars = await resp.data;
+    console.log(chars);
     for (let i = 0; i < chars.length; i++) {
         let Node = document.importNode(container.content, true);
         Node.querySelector("#card_image").src = `data:image/gif;base64,${chars[i].image}`;
@@ -15,6 +17,15 @@ async function getCharacters() {
         Node.querySelector("#card_description").innerHTML = `${chars[i].description}`;
         Node.querySelector("#readMore_button").href = `card.html?${chars[i].id}`;
         Node.querySelector("#readMore_button").innerText = `See the Character`;
-        document.body.appendChild(Node)
+        document.body.prepend(Node)
     }
+    }
+    catch {
+        if (chars[i].image === "") {
+            let Node = document.importNode(container.content, true);
+            Node.querySelector("#card_image").src = `./src/assets/not-found.jpg`;
+        }
+    }
+    document.querySelector("body").setAttribute('class', 'bg-gray-800 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-11/12 m-auto gap-4')
+    document.querySelector("#newButton").classList.remove('hidden')
 }
