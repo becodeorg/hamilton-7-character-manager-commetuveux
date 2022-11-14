338bd85b9ab5a6705d09adbd11987a00ebd99672
GeorisVal <georis.val@gmail.com>
2022-11-14 16:15:30 +0100

    Fixed indenting

--- src/js/manager.js
+++ src/js/manager.js
@@ -52,8 +52,8 @@ async function getCharacters() {
     catch (error) {
         console.log(error);
         }
-    }
-    
+}
+
     // Swiper part, broken for now
     // document.querySelector("#swiper").src = "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js";
     /* const swiper = new swiper('.swiper', {
