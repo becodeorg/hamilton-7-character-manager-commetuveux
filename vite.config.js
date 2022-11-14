c64ffab575590d12f746eac4724838e8a3784e37
GeorisVal <georis.val@gmail.com>
2022-11-14 14:05:55 +0100

    Testing Dist in base

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/hamilton-7-character-manager-commetuveux/', 
+    base: '/hamilton-7-character-manager-commetuveux/dist/', 
     plugins: []
 })
