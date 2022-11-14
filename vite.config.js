ad626536c75a437a7f9d1598b9a5bb148b11e0cc
GeorisVal <georis.val@gmail.com>
2022-11-14 14:40:29 +0100

    Fixed multi-pages on deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+    build: {
+        targer: 'esnext',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                form: resolve(__dirname, "form.html"),
+                card: resolve(__dirname, "card.html"),
+            }
+        }
+    },
     base: '/hamilton-7-character-manager-commetuveux/dist/', 
-    plugins: []
+    plugins: [topLevelAwait({
+        promiseExportName: "__tla",
+        promiseImportName: i => `__tla_${i}`
+    })
+    ],
 })
