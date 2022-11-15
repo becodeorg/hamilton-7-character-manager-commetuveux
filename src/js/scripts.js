53d92cfe4cf577d22c7fa85d2cb976c5c7c180db
Elhya13 <aureliewinkin@gmail.com>
2022-11-15 15:48:04 +0100

    " tik tik"

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,7 +4,7 @@ import VanillaTilt from "vanilla-tilt"
 async function getCharacters() {
     const url_string = window.location.href;
     const url = new URL(url_string);
-    const urlId = url.search.split('?')[1];
+    const urlId = url.search.split("?")[1];
     try {
     const resp = await axios.get(`https://character-database.becode.xyz/characters/${urlId}`);
     const chars = await resp.data;
@@ -13,28 +13,28 @@ async function getCharacters() {
     console.log(document.title);
     document.title = `${chars.name}'s Card`
     console.log(document.title);
-    const charName = document.querySelector('#card_charName');
-    const charNickname = document.querySelector('#card_nickName');
-    const charDesc = document.querySelector('#card_description');
-    const charImg = document.querySelector('#card_image');
+    const charName = document.querySelector("#card_charName");
+    const charNickname = document.querySelector("#card_nickName");
+    const charDesc = document.querySelector("#card_description");
+    const charImg = document.querySelector("#card_image");
     const dropdownEdit = document.querySelector("#dropdown_Edit");
     //const dropdownDel = document.querySelector("dropdown_Delete");
-    charImg.setAttribute('src', `data:image;base64,${chars.image}`)
+    charImg.setAttribute("src", `data:image;base64,${chars.image}`)
     charName.innerText = chars.name;
     charNickname.innerText = chars.shortDescription;
     charDesc.innerHTML = chars.description;
     dropdownEdit.href = `form.html?${urlId}`
     }
     catch (error) {
-    console.log('There was une couille dans le pâté')
-    const charName = document.querySelector('#card_charName');
-    const charNickname = document.querySelector('#card_nickName');
-    const charDesc = document.querySelector('#card_description');
-    const charImg = document.querySelector('#card_image');
-    charImg.setAttribute('src', `./src/assets/not-found.jpg`)
-    charName.innerText = 'Invalid ID';
-    charNickname.innerText = 'Invalid ID';
-    charDesc.innerText = 'The ID you have entered is not valid, make sure to check out your link !';
+    console.log("There was une couille dans le pâté")
+    const charName = document.querySelector("#card_charName");
+    const charNickname = document.querySelector("#card_nickName");
+    const charDesc = document.querySelector("#card_description");
+    const charImg = document.querySelector("#card_image");
+    charImg.setAttribute("src", `./src/assets/not-found.jpg`)
+    charName.innerText = "Invalid ID";
+    charNickname.innerText = "Invalid ID";
+    charDesc.innerText = "The ID you have entered is not valid, make sure to check out your link !";
     }
 }
 
@@ -42,7 +42,7 @@ async function getCharacters() {
 document.getElementById("dropdown_Delete").addEventListener("click", async () => {
     const url_string = window.location.href;
     const url = new URL(url_string);
-    const urlId = url.search.split('?')[1];
+    const urlId = url.search.split("?")[1];
     const resp = await axios.get(`https://character-database.becode.xyz/characters/${urlId}`);
     const chars = await resp.data;
     if (confirm(`You are about to delete ${chars.name}'s entry. Are you sure you want to continue? This cannot be undone.`)) {
